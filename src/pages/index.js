c7e44d840614e730e6b684290f27dfd9fcdcbcd2
Wael <wael@unik.ma>
2018-07-22 23:35:44 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Sonia Teaching Tools</h1>
+  </Layout>
+)
+
+export default IndexPage
